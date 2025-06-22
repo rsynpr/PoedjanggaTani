b3ed4f2e2880a46e913ee2c770b56b13f3e0195b
rsynpr <nizarpradipa@gmail.com>
2025-06-23 01:17:19 +0700

    fix4

--- controllers/transactionController.js
+++ controllers/transactionController.js
@@ -1,29 +1,29 @@
-// controllers/transactionController.js
-const Transaction = require('../models/Transaction');
+// // controllers/transactionController.js
+// const Transaction = require('../models/Transaction');
 
-exports.createTransaction = async (req, res) => {
-  const { items, total, metodePembayaran } = req.body;
-  try {
-    const newTransaction = await Transaction.create({
-      userId: req.user._id,
-      items,
-      total,
-      metodePembayaran
-    });
-    res.status(201).json(newTransaction);
-  } catch (err) {
-    res.status(500).json({ message: 'Gagal menyimpan transaksi', error: err.message });
-  }
-};
+// exports.createTransaction = async (req, res) => {
+//   const { items, total, metodePembayaran } = req.body;
+//   try {
+//     const newTransaction = await Transaction.create({
+//       userId: req.user._id,
+//       items,
+//       total,
+//       metodePembayaran
+//     });
+//     res.status(201).json(newTransaction);
+//   } catch (err) {
+//     res.status(500).json({ message: 'Gagal menyimpan transaksi', error: err.message });
+//   }
+// };
 
-exports.getUserTransactions = async (req, res) => {
-  try {
-    const transactions = await Transaction.find({ userId: req.user._id }).populate({
-      path: 'items.productId',
-      model: 'Product'
-    });
-    res.json(transactions);
-  } catch (err) {
-    res.status(500).json({ message: 'Gagal mengambil transaksi', error: err.message });
-  }
-};
+// exports.getUserTransactions = async (req, res) => {
+//   try {
+//     const transactions = await Transaction.find({ userId: req.user._id }).populate({
+//       path: 'items.productId',
+//       model: 'Product'
+//     });
+//     res.json(transactions);
+//   } catch (err) {
+//     res.status(500).json({ message: 'Gagal mengambil transaksi', error: err.message });
+//   }
+// };
--- routes/products.js
+++ routes/products.js
@@ -1,11 +1,11 @@
-// routes/products.js
-const express = require('express');
-const router = express.Router();
-const { getAllProducts, createProduct, getProductById } = require('../controllers/productController');
-const protect = require('../middleware/authMiddleware');
+// // routes/products.js
+// const express = require('express');
+// const router = express.Router();
+// const { getAllProducts, createProduct, getProductById } = require('../controllers/productController');
+// const protect = require('../middleware/authMiddleware');
 
-router.get('/', getAllProducts);
-router.post('/', protect, createProduct); // bisa dibatasi admin
-router.get('/:id', getProductById);
+// router.get('/', getAllProducts);
+// router.post('/', protect, createProduct); // bisa dibatasi admin
+// router.get('/:id', getProductById);
 
-module.exports = router;
+// module.exports = router;
